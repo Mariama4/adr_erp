478393cd204207c3c8cc8dd9679193f16defa22d
georgijtaskabulov <mariama4@mail.ru>
2025-05-13 14:32:42 +0300

    change logic for default value in entry_type

--- adr_erp/base/doctype/expense_item/expense_item.js
+++ adr_erp/base/doctype/expense_item/expense_item.js
@@ -1,8 +1,11 @@
 // Copyright (c) 2025, GeorgyTaskabulov and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Expense item", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Expense item", {
+	onload: function (frm) {
+		// Только для новых документов и только если ещё не установлено
+		if (frm.is_new() && !frm.doc.entry_type) {
+			frm.set_value("entry_type", "Credit");
+		}
+	},
+});
