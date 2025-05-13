5818849188cbbad974c646c827572bd6a1407919
georgijtaskabulov <mariama4@mail.ru>
2025-05-13 13:53:39 +0300

    Automatically add all selections so that they can be removed later

--- adr_erp/budget/doctype/organization_bank_rules/organization_bank_rules.js
+++ adr_erp/budget/doctype/organization_bank_rules/organization_bank_rules.js
@@ -1,8 +1,36 @@
 // Copyright (c) 2025, GeorgyTaskabulov and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Organization-Bank Rules", {
-// 	refresh(frm) {
+frappe.ui.form.on("Organization-Bank Rules", {
+	refresh(frm) {
+		// только для новой формы
+		if (!frm.is_new()) return;
 
-// 	},
-// });
+		// получаем все Expense Item
+		frappe
+			.call({
+				method: "frappe.client.get_list",
+				args: {
+					doctype: "Expense Item",
+					fields: ["name"], // здесь важно: берем поле name
+					limit_page_length: 0,
+				},
+			})
+			.then((r) => {
+				const items = r.message || [];
+
+				// очищаем таблицу
+				frm.clear_table("available_expense_items");
+
+				// создаём новую строку для каждого элемента
+				items.forEach((item) => {
+					const row = frm.add_child("available_expense_items");
+					row.link_expense_item = item.name;
+					row.is_transit = 0; // по умолчанию
+				});
+
+				// перерисовываем сетку
+				frm.refresh_field("available_expense_items");
+			});
+	},
+});
