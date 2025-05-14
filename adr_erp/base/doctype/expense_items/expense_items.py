9ab94eaf7dceb71ceae6ff58c74c3834a5fca85d
georgijtaskabulov <mariama4@mail.ru>
2025-05-14 15:35:49 +0300

    add field mandatory for expense item to prohibit the deletion of mandatory expense items

--- adr_erp/base/doctype/expense_items/expense_items.py
+++ adr_erp/base/doctype/expense_items/expense_items.py
@@ -1,9 +1,16 @@
 # Copyright (c) 2025, GeorgyTaskabulov and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
+from frappe import _
 from frappe.model.document import Document
 
 
 class ExpenseItems(Document):
-	pass
+	def on_trash(self):
+		# Если установлен флаг is_mandatory, запретить удаление
+		if self.get("is_mandatory"):
+			frappe.throw(
+				_("Cannot delete mandatory Expense Item: {0}").format(self.name),
+				title=_("Deletion Not Allowed"),
+			)
