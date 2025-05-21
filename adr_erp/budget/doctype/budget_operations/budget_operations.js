5a60b55c7899d4d6191a2eae79a41b165d4dfe27
georgijtaskabulov <mariama4@mail.ru>
2025-05-21 12:52:24 +0300

    wtf precommit

--- adr_erp/budget/doctype/budget_operations/budget_operations.js
+++ adr_erp/budget/doctype/budget_operations/budget_operations.js
@@ -1,28 +1,28 @@
 // Copyright (c) 2025, GeorgyTaskabulov and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Budget Operations', {
+frappe.ui.form.on("Budget Operations", {
 	// При изменении поля expense_item
 	expense_item: function (frm) {
 		if (frm.doc.expense_item) {
 			// Получаем документ Expense item по значению поля expense_item
-			frappe.db.get_doc('Expense Items', frm.doc.expense_item).then((doc) => {
+			frappe.db.get_doc("Expense Items", frm.doc.expense_item).then((doc) => {
 				// Если у полученного документа стоит галочка is_transit, показываем поле, иначе скрываем
-				frm.toggle_display('recipient_of_transit_payment', !!doc.is_transit);
+				frm.toggle_display("recipient_of_transit_payment", !!doc.is_transit);
 			});
 		} else {
 			// Если поле expense_item не заполнено, скрываем поле recipient_of_transit_payment
-			frm.toggle_display('recipient_of_transit_payment', false);
+			frm.toggle_display("recipient_of_transit_payment", false);
 		}
 	},
 	// При загрузке/обновлении формы
 	refresh: function (frm) {
 		if (frm.doc.expense_item) {
-			frappe.db.get_doc('Expense Items', frm.doc.expense_item).then((doc) => {
-				frm.toggle_display('recipient_of_transit_payment', !!doc.is_transit);
+			frappe.db.get_doc("Expense Items", frm.doc.expense_item).then((doc) => {
+				frm.toggle_display("recipient_of_transit_payment", !!doc.is_transit);
 			});
 		} else {
-			frm.toggle_display('recipient_of_transit_payment', false);
+			frm.toggle_display("recipient_of_transit_payment", false);
 		}
 	},
 });
